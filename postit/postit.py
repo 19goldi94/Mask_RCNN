acef8e8cde2a3aa58c259bd3cc73bb89e6064a83
19goldi94 <46821524+19goldi94@users.noreply.github.com>
2019-04-03 12:43:12 +0200

    Update postit.py

--- postit/postit.py
+++ postit/postit.py
@@ -1,1 +1,15 @@
 
+import os
+import sys
+import json
+import datetime
+import numpy as np
+import skimage.draw
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn.config import Config
+from mrcnn import model as modellib, utils
